8e17c09d299ec8e280b2466e07ddb7669f08d11c
Sergey Ryazanov <majo78@mail.ru>
2024-06-27 20:00:02 +0300

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	pb "servergrpc/api/proto/pb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedSimpleServiceServer
+}
+
+func (s *server) Echo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: "Echo: " + in.GetMessage()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterSimpleServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
